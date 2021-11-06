6bba810b2abdfdb6ad60e57e54125a01009a00c5
LaukikOnChromeBook <22937426+LRagji@users.noreply.github.com>
2021-11-06 16:08:23 +0530

    Completed write operation

--- source/n-dimension-shared-partitioned-sorted-set.ts
+++ source/n-dimension-shared-partitioned-sorted-set.ts
@@ -1,20 +1,23 @@
 import { IBulkResponse, IError, IPurgeableSortedSetFamily, ISortedStringData } from "./index";
 import dimensionalHelper from "@stdlib/ndarray";
+import Crypto from "crypto";
 
 export class NDimensionalSharedPartitionedSortedSet {
     private partitionShape: bigint[];
     private partitionNameSeperator: string;
-    private partitionShapeInterger: number[];
+    private partitionShapeInteger: number[];
     private shardResolver: (details: IPatitionDetails) => IPurgeableSortedSetFamily<ISortedStringData>;
+    private settingsHash: string;
 
     constructor(partitionShape: bigint[], shardResolver: (details: IPatitionDetails) => IPurgeableSortedSetFamily<ISortedStringData>, partitionNameSeperator = '-') {
         if (partitionShape.length === 0) {
             throw new Error(`Invalid parameter "partitionShape" cannot be of length zero.`);
         }
         this.partitionShape = partitionShape;
-        this.partitionShapeInterger = partitionShape.map(e => parseInt(e.toString()));
+        this.partitionShapeInteger = partitionShape.map(e => parseInt(e.toString()));
         this.partitionNameSeperator = partitionNameSeperator;
         this.shardResolver = shardResolver;
+        this.settingsHash = this.objectHash({ "partitionNameSeperator": partitionNameSeperator, "partitionShape": partitionShape.join(partitionNameSeperator) });
     }
 
     async write(data: IDimentionalData[]): Promise<IBulkResponse<IDimentionalData[], IError<IDimentionalData>[]>> {
@@ -29,7 +32,7 @@ export class NDimensionalSharedPartitionedSortedSet {
                 const partitionStart = this.partitionShape.map((ps, psIdx) => element.dimensions[psIdx] - (element.dimensions[psIdx] % ps));
                 const partitionName = this.partitionNameBuilder(partitionStart);
                 const relativeDimensions = partitionStart.map((ps, psIdx) => element.dimensions[psIdx] - ps);
-                const score = BigInt(dimensionalHelper.sub2ind(this.partitionShapeInterger, ...relativeDimensions.map(e => parseInt(e.toString())), { order: 'row-major' }));
+                const score = BigInt(dimensionalHelper.sub2ind(this.partitionShapeInteger, ...relativeDimensions.map(e => parseInt(e.toString())), { order: 'row-major' }));
                 const existingData = partitionData.get(partitionName) || { data: new Array<ISortedStringData>(), partitiondetails: { name: partitionName, startDimensions: partitionStart }, rawData: new Array<IDimentionalData>() };
                 existingData.data.push({ score: score, setName: partitionName, payload: element.payload, bytes: element.bytes });
                 existingData.rawData.push(element);
@@ -42,17 +45,31 @@ export class NDimensionalSharedPartitionedSortedSet {
             const info = partitionData.get(partitionName)!;
             const shard = await this.shardResolver(info.partitiondetails);
             const results = await shard.upsert(info.data);
-            results.succeeded.map(e => ({ payload: e.payload, dimen }))
+            returnObject.failed = results.failed.map(failedElement => {
+                const failedIndex = info.rawData.findIndex(e => e.payload === failedElement.data.payload);
+                if (failedIndex != -1) {
+                    const failedItem = info.rawData.splice(failedIndex, 1)[0];
+                    return { error: failedElement.error, data: failedItem };
+                }
+                else {
+                    return { error: new Error(`Cannot find index for payload ${failedElement.data.payload} which has following error ${failedElement.error?.message}`), data: { payload: failedElement.data.payload, dimensions: [] } };
+                }
+            });
+            returnObject.succeeded = returnObject.succeeded.concat(info.rawData);
         }
         return returnObject;
     }
 
-    partitionNameBuilder(partitionStart: bigint[]): string {
-        return partitionStart.join(this.partitionNameSeperator);
-    }
-
     rangeRead(start: Array<bigint>, end: Array<bigint>): Promise<IError<IDimentionalData[]>> {
         throw new Error("Not implmented");
+    }
+
+    private partitionNameBuilder(partitionStart: bigint[]): string {
+        return `${this.settingsHash}${this.partitionNameSeperator}${partitionStart.join(this.partitionNameSeperator)}`;
+    }
+
+    private objectHash(settings: object): string {
+        return Crypto.createHash("sha256").update(JSON.stringify(settings), "binary").digest("hex");
     }
 }
 
